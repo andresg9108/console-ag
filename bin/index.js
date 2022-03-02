49ae67e8eb789aea6f4cdb922259a3816bacb90d
Andrés González <andresg9108@outlook.com>
2022-03-02 09:35:47 -0500

    Modifying the initial structure.

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,21 @@
 #!/usr/bin/env node
-var oSplashscreen = require('../modules/splashscreen.js');
 var oMain = require('../modules/index.js');
 
 try{
-	oSplashscreen.start();
-	oMain.start();
+	const aArgs = process.argv.splice(process.execArgv.length + 2);
+	const sCommand = aArgs[0];
+
+	switch (sCommand) {
+		case 'start':
+			oMain.start();
+			break;
+		case 'help':
+			break;
+		default:
+			throw(`The instruction "${sCommand}" is not recognized. Run "console-ag help" to get help.`);
+			break;
+	}
+
 }catch(e){
 	console.log(` Error:\n ${e}`);
 }
